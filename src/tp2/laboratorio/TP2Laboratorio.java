a84c7fccc8ce5b941523dd6378537ad1debbc726
maximiliano macia <m.macia186@gmail.com>
2024-08-22 12:42:33 -0300

    asteriscos

--- src/tp2/laboratorio/TP2Laboratorio.java
+++ src/tp2/laboratorio/TP2Laboratorio.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp2.laboratorio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Netbook
- */
+
 public class TP2Laboratorio {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         
@@ -25,8 +16,8 @@ public class TP2Laboratorio {
         Persona p2 = new Persona("Fabri", "Zalazar", 1.80, 23, r);
         Persona p = new Persona("Maxi", "Macia", 1.85, 36, FIT);
         
-        System.out.println("Hola " + p2.getNombre());
-        p2.usarReloj();
+        //System.out.println("Hola " + p2.getNombre());
+        //p2.usarReloj();
         System.out.println("Hola " + p.getNombre());
         p.usarReloj();
         
